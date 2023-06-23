60024de8ec5eeff459c339843409eb5617d2380a
Konstantin Ligus <kostya1989com@gmail.com>
2023-06-23 09:34:56 +0300

    feat: Change react root render change router

--- packages/frontend/src/modules/navigation/index.tsx
+++ packages/frontend/src/modules/navigation/index.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import HomePageContainer from '../home';
+import { Route, BrowserRouter, Routes } from 'react-router-dom';
 import { APP_KEYS } from '../common/consts';
+import HomePageContainer from '../home';
+import TodoPage from '../todo';
+import TodosPage from '../todos';
 
 export const MainRouter = () => (
-  <Router>
-    <Switch>
-      <Route component={HomePageContainer} path={APP_KEYS.ROUTER_KEYS.ROOT} />
-    </Switch>
-  </Router>
+  <BrowserRouter>
+    <Routes>
+      <Route element={<HomePageContainer />} path={APP_KEYS.ROUTER_KEYS.ROOT} />
+      <Route element={<TodoPage />} path={APP_KEYS.ROUTER_KEYS.TODO} />
+      <Route element={<TodosPage />} path={APP_KEYS.ROUTER_KEYS.TODOS} />
+    </Routes>
+  </BrowserRouter>
 );
