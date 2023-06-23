c63e4a658862cb58325cbdfe86caf9f15a15ba8a
Konstantin Ligus <kostya1989com@gmail.com>
2023-06-23 09:46:00 +0300

    feat: Make todo page

---
+++ packages/frontend/src/modules/todo/index.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import { TodoItem } from '../common/components/TodoItem';
+
+const TodoPage = () => <TodoItem title="Buy lemons" description="I got it" />;
+
+export default TodoPage;
