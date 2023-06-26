dce1c8ee6c0fbe9317b187a2c348532138bdee09
Konstantin Ligus <kostya1989com@gmail.com>
2023-06-26 21:34:49 +0300

    feat: Create createTodo page

---
+++ packages/frontend/src/modules/createTodo/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Container } from '../common/components/Container';
+import { TodoForm } from '../common/components/TodoForm';
+
+const CreateTodoPage = () => (
+  <Container>
+    <TodoForm />
+  </Container>
+);
+
+export default CreateTodoPage;
